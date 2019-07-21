89a9fa3b831ed0f2fb1676b60d21482df108a12a
한승우 <tuguri8@gmail.com>
2019-07-21 22:32:02 +0900

    #5 언팔로우 테스트 코드 추가

--- src/test/java/com/timeline/api/application/FollowServicemplTest.java
+++ src/test/java/com/timeline/api/application/FollowServicemplTest.java
@@ -10,13 +10,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@Rollback(value = true)
 public class FollowServicemplTest {
 
     @Autowired
@@ -31,12 +33,24 @@ public class FollowServicemplTest {
     @Before
     public void setUp() {
         userService.signUpUser("tuguri5", "김길동", "asdf12");
-        userService.signUpUser("tuguri6", "홍길동", "asdf12");
     }
 
     @Test
     public void 팔로우_테스트() {
-        followService.followUser("tuguri5", "tuguri6");
-        assertThat(followRepository.findByUserId("tuguri5").isPresent()).isTrue();
+        followService.followUser("tuguri87", "tuguri5");
+        assertThat(followRepository.findByUserId("tuguri87").isPresent()).isTrue();
+        followRepository.delete(followRepository.findByUserId("tuguri87").get());
+    }
+
+    @Test
+    public void 언팔로우_테스트() {
+        followService.followUser("tuguri87", "tuguri5");
+        followService.unFollowUser("tuguri87", "tuguri5");
+        assertThat(followRepository.findByUserId("tuguri87").isPresent()).isFalse();
+    }
+
+    @After
+    public void after() {
+        accountRepository.delete(accountRepository.findByUserId("tuguri5").get());
     }
 }
